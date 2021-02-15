84102b944186b0ce4faa1562273c068e58d1b640
jkohhokj <50003626+jkohhokj@users.noreply.github.com>
2021-02-15 13:18:58 -0600

    Update Vigenere.py

--- Vigenere.py
+++ Vigenere.py
@@ -8,15 +8,19 @@ def vigenereEncode(message,key):
     numToLet = dict(zip([x for x in range(26)],list(string.ascii_uppercase)))
     letToNum = dict(zip(list(string.ascii_uppercase),[x for x in range(26)]))
     # set up the table
-    decoded = []
+    encoded = []
     for charIndex in range(len(message)):
-        #print("first",letToNum[str(message[charIndex])])
-        #print("second",letToNum[str(key[keyIndex])])
-        #print("newNum",letToNum[message[charIndex]]+letToNum[key[keyIndex%len(key)]])
-        #print("newLet",numToLet[(letToNum[message[charIndex]]+letToNum[key[charIndex%len(key)]])%26])
-        decoded.append(numToLet[(letToNum[message[charIndex]]+letToNum[key[charIndex%len(key)]])%26])
-    # translate the message
-    return ''.join(decoded)
+        if message[charIndex] in string.ascii_uppercase:
+            #human readable one is commented
+            #firstLetNum = letToNum[str(message[charIndex])]
+            #secondLetNum = letToNum[str(key[charIndex%len(key)])]
+            #newNum = (firstLetNum + secondLetNum)%26
+            #newLet = numToLet[newNum]
+            #encoded.append(newLet)
+            encoded.append(numToLet[(letToNum[message[charIndex]]+letToNum[key[charIndex%len(key)]])%26])
+        else:
+            encoded.append(message[charIndex])
+    return ''.join(encoded)
 def vigenereDecode(message,key): # literally change the plus to minus
     message = message.upper()
     key = key.upper()
@@ -26,32 +30,24 @@ def vigenereDecode(message,key): # literally change the plus to minus
     decoded = []
     for charIndex in range(len(message)):
         if message[charIndex] in string.ascii_uppercase:
-        #print("first",letToNum[str(message[charIndex])])
-        #print("second",letToNum[str(key[keyIndex])])
-        #print("newNum",letToNum[message[charIndex]]+letToNum[key[keyIndex%len(key)]])
-        #print("newLet",numToLet[(letToNum[message[charIndex]]+letToNum[key[charIndex%len(key)]])%26])
+            # human readable one is commented
+            #firstLetNum = letToNum[str(message[charIndex])]
+            #secondLetNum = letToNum[str(key[charIndex%len(key)])]
+            #newNum = (firstLetNum - secondLetNum)%26
+            #newLet = numToLet[newNum]
+            #encoded.append(newLet)
             decoded.append(numToLet[(letToNum[message[charIndex]]-letToNum[key[charIndex%len(key)]])%26])
         else:
             decoded.append(message[charIndex])
-    # translate the message
     return ''.join(decoded)
-def autoVigenere(message,keyword,min=2,max=5): #3=.8;4=21;5=10:40;6=4:40:00
+def autoVigenere(message,keyword,min=2,max=5,showTime=False): #3=.8;4=21;5=10:40;6=4:40:00
     old = datetime.now()
-    #modMessage = []
-    #for word in message.split():
-    #    if len(word) == len(keyword):
-    #        modMessage.append(word)
-    #modMessage = ' '.join(modMessage)
-    #print(modMessage) #change message to modMessage in for loop in message
     message = message.upper()
-
     max += 1
     for length in range(min,max):
         for key in bruteForceLetter(length):
-            #print("keyword encoded: ",vigenereEncode(keyword,key),' ',"")
-            #print(vigenereEncode(keyword,key))
-            #print(message)
             if vigenereEncode(keyword,key) in message:
                 print("decoded: ",vigenereDecode(message,key)," ","key: ",key)
-                print(datetime.now()-old)
+                if showTime:
+                    print(datetime.now()-old)
                 return vigenereDecode(message,key)
